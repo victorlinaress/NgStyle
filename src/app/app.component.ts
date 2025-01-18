c2f9879f50c49b2bf23a31dbb9662fccebcac182
unknown <victorbr8@outlook.com>
2025-01-18 18:50:03 -0300

    atualizacao

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,32 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: false,
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'], // Corrigido 'styleUrl' para 'styleUrls'
 })
 export class AppComponent {
-fontSize: number = 15;
-textColor: 'white' | 'orange' = 'white'
+  fontSize: number = 15;
+  textColor: 'white' | 'orange' = 'white';
+  buttonText: 'Branco' | 'Laranja' = 'Laranja';
 
-increaseFontSize() {
-  this.fontSize +=5
-}
+  stylesString: string = 'font-size: 15px; color: white;'; // Adicionado ponto e vírgula ao final
+  stylesObj = {
+    'font-size': this.fontSize + 'px',
+    'color': this.textColor,
+  };
+
+  increaseFontSize() {
+    this.fontSize += 5;
+    this.stylesString = `font-size: ${this.fontSize}px; color: ${this.textColor};`;
+  }
+
+  toggleFontColor() {
+    if (this.textColor === 'white') {
+      this.textColor = 'orange';
+      this.buttonText = 'Branco';
+    } else {
+      this.textColor = 'white';
+      this.buttonText = 'Laranja';
+    }
+    this.stylesString = `font-size: ${this.fontSize}px; color: ${this.textColor};`;
+  }
 }
